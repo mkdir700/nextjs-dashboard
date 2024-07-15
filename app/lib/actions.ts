031a9d4776246b347cb6118db704b42c865de2fc
mkdir700 <mkdir700@gmail.com>
2024-07-15 20:08:47 +0800

    使用 forms 和 server actions

---
+++ app/lib/actions.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(), // coerce is used to convert the value to a number
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function createInvoice(formData: FormData) {
+  const {customerId, amount, status} = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // Note: If the forms have many fileds,
+  // it's better to use the `entries()` method with JavaScript's `Object.fromEntries()` method
+  // const rawFormData = Object.fromEntries(formData.entries());
+  
+  // storing values in cents
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO INVOICES (CUSTOMER_ID, AMOUNT, STATUS, DATE)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // I need to update the cache for the invoices page
+  // because the new invoice will be displayed there
+  // so I need to clear this cache and tigger a new request to the server using the `revalidatePath` function.
+  revalidatePath('/dashboard/invoices');
+
+  // i want to redirect the user back to the `/dashboard/invoices` page,
+  // so i can use the `redirect` function to do that.
+  redirect('/dashboard/invoices');
+}
